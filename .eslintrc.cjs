3633705c3b4ad00ce3afb75f7f110bf424e494a0
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-07-14 12:57:07 +0700

    feat: add packages, rules eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,61 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  extends: [
+    "eslint:recommended",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:storybook/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "jsx-a11y"],
+  settings: {
+    "import/resolver": {
+      typescript: true,
+    },
+  },
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    quotes: "off",
+    "linebreak-style": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    "import/prefer-default-export": "off",
+    "react/require-default-props": "off",
+    "react/jsx-filename-extension": [
+      "error",
+      {
+        extensions: [".tsx", ".ts"],
+      },
     ],
   },
-}
+  "prettier/prettier": [
+    "error",
+    {
+      singleQuote: false,
+      parser: "flow",
+    },
+  ],
+  globals: {
+    React: "readonly",
+    ReactDOM: "readonly",
+  },
+  overrides: [
+    {
+      files: ["**/*.stories.@(js|jsx|ts|tsx)"],
+      rules: {
+        "react-hooks/rules-of-hooks": "off",
+      },
+    },
+  ],
+};
