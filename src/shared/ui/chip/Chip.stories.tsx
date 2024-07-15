ca3cbf8d02d44a39aa811a7d4c27f4d6b14790fa
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-07-16 01:49:07 +0700

    feat(ui): add chip & label

---
+++ src/shared/ui/chip/Chip.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Chip } from "./Chip";
+
+const meta: Meta<typeof Chip> = {
+  title: "Chip",
+  component: Chip,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    variant: {
+      control: { type: "radio" },
+      options: ["default", "info", "success", "error", "warning"],
+    },
+    size: {
+      control: { type: "radio" },
+      options: ["small", "medium", "large"],
+    },
+  },
+  args: {
+    label: "Отправлен",
+    variant: "info",
+    size: "medium",
+    className: "",
+  },
+} satisfies Meta<typeof Chip>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
---
+++ src/shared/ui/label/Label.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Label } from "./Label";
+import { Input } from "../input/Input";
+
+const meta: Meta<typeof Label> = {
+  title: "Label",
+  component: Label,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    direction: {
+      control: { type: "radio" },
+      options: ["up", "down", "left", "right"],
+    },
+  },
+  args: {
+    direction: "up",
+    text: "Здесь могла быть ваша реклама",
+    className: "",
+  },
+} satisfies Meta<typeof Label>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const WithChildren: Story = {
+  args: {
+    children: <Input placeholder="Введите имя" />,
+  },
+};
+
+export const Default: Story = {
+  args: {},
+};
