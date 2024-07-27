d2a13e36c1437a23ba2225e76afab37d64316c6a
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-07-27 17:30:21 +0900

    fix(ui): change props

--- src/shared/ui/box/Box.tsx
+++ src/shared/ui/box/Box.tsx
@@ -2,25 +2,27 @@ import { forwardRef, HTMLAttributes, ReactNode } from "react";
 import clsx from "clsx";
 import styles from "./Box.module.scss";
 
-interface BoxProps extends HTMLAttributes<HTMLElement> {
+interface BoxProps extends HTMLAttributes<HTMLDivElement> {
   className?: string;
   children: ReactNode;
   direction?: "up" | "down" | "left" | "right" | "center";
-  text: string;
-  size?: "small" | "medium" | "large";
+  text?: string;
+  fullWidth?: boolean;
   color?: "blue" | "white";
 }
 
-const Box = forwardRef<HTMLLabelElement, BoxProps>(
-  ({ className, direction = "up", size = "small", color = "white", children, text, ...props }, ref) => (
+const Box = forwardRef<HTMLDivElement, BoxProps>(
+  ({ className, direction = "up", color = "white", children, text, fullWidth, ...props }, ref) => (
     <div
+      ref={ref}
       className={clsx(styles.box, className, {
         [styles[direction]]: direction,
-        [styles[size]]: size,
         [styles[color]]: color,
+        [styles.fullWidth]: fullWidth,
       })}
       {...props}
     >
+      {text && <span className={styles.box__text}>{text}</span>}
       {children}
     </div>
   ),
