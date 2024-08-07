d2f3a035aa86c7b817ab6f38030e6878950b80cd
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-08-07 16:28:52 +0900

    fix(store): add type & new void

--- src/pages/home/components/mainPanel/MainPanel.store.tsx
+++ src/pages/home/components/mainPanel/MainPanel.store.tsx
@@ -1,13 +1,28 @@
 import { create } from "zustand";
 
-interface WayMenu {
+export interface Location {
+  city: string;
+  street: string;
+  house: string;
+  station: string;
+  time: string;
   date: string;
-  from: string;
-  to: string;
 }
 
-interface WayMenuReturn {
-  have: boolean;
+export interface WayDetails {
+  id: number;
+  way: string;
+  wayNumber: string;
+  whoArive: string;
+  price: number;
+  seatsAvailable: string;
+  booking: string;
+  from: Location;
+  to: Location;
+}
+
+interface WayMenu {
+  have?: boolean;
   date: string;
   from: string;
   to: string;
@@ -16,9 +31,14 @@ interface WayMenuReturn {
 interface Store {
   saleTicket: {
     way: WayMenu;
-    returnWay: WayMenuReturn;
-    setReturnWay: (data: WayMenuReturn) => void;
+    returnWay: WayMenu;
+    activeWay: WayDetails | null;
+    wayDetails: WayDetails[];
+
+    setReturnWay: (data: WayMenu) => void;
     setWay: (data: WayMenu) => void;
+    setWayDetails: (data: WayDetails[]) => void;
+    setActiveWay: (way: WayDetails | null) => void;
   };
 }
 
@@ -35,10 +55,67 @@ const useMainStore = create<Store>((set) => ({
       from: "",
       to: "",
     },
+    activeWay: null,
+    wayDetails: [
+      {
+        id: 1,
+        way: "Москва - Кемерово",
+        wayNumber: "7345",
+        whoArive: "ООО “Кузбасские междугородние перевозки”",
+        price: 1276,
+        seatsAvailable: "22",
+        booking: "5",
+        from: {
+          city: "Москва",
+          street: "ул.Ленина",
+          house: "67",
+          station: "ЖД Вокзал",
+          time: "13:20",
+          date: "2022-05-01",
+        },
+        to: {
+          city: "Кемерово",
+          street: "пр.Кузнецкий",
+          house: "81",
+          station: "",
+          time: "17:50",
+          date: "2022-05-01",
+        },
+      },
+      {
+        id: 2,
+        way: "Новосибирск - Кемерово",
+        wayNumber: "7345",
+        whoArive: "ООО “Кузбасские междугородние перевозки”",
+        price: 1276,
+        seatsAvailable: "22",
+        booking: "5",
+        from: {
+          city: "Новосибирск",
+          street: "ул.Ленина",
+          house: "67",
+          station: "ЖД Вокзал",
+          time: "13:20",
+          date: "2022-05-01",
+        },
+        to: {
+          city: "Кемерово",
+          street: "пр.Кузнецкий",
+          house: "81",
+          station: "",
+          time: "17:50",
+          date: "2022-05-01",
+        },
+      },
+    ],
 
-    setReturnWay: (data: WayMenuReturn) =>
+    setReturnWay: (data: WayMenu) =>
       set((state) => ({ saleTicket: { ...state.saleTicket, returnWay: data } })),
     setWay: (data: WayMenu) => set((state) => ({ saleTicket: { ...state.saleTicket, way: data } })),
+    setWayDetails: (data: WayDetails[]) =>
+      set((state) => ({ saleTicket: { ...state.saleTicket, wayDetails: data } })),
+    setActiveWay: (way: WayDetails | null) =>
+      set((state) => ({ saleTicket: { ...state.saleTicket, activeWay: way } })),
   },
 }));
 
