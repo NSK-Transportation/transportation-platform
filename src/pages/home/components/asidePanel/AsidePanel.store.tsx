9fc80a52a95947e4a047fb0d92952350fea2f91c
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-08-17 18:50:13 +0900

    fix(store): refactor

--- src/pages/home/components/asidePanel/AsidePanel.store.tsx
+++ src/pages/home/components/asidePanel/AsidePanel.store.tsx
@@ -1,4 +1,6 @@
 import { create } from "zustand";
+import { createJSONStorage, devtools, persist } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 
 interface Link {
   [x: string]: any;
@@ -11,41 +13,80 @@ interface Store {
   linksNames: { [key: string]: string };
   activeLink: string | null;
   selectLink: (text: string) => void;
+  setActiveLinkByRoute: () => void;
 }
 
-const useAsideStore = create<Store>((set, get) => ({
-  links: {
-    cashier: [
-      { to: "/home/sale-ticket?step=0", text: "Продажа билетов" },
-      { to: "/2", text: "Возврат билетов" },
-    ],
-    dispatcher: [{ to: "/3", text: "Управление рейсами" }],
-    administrator: [
-      { to: "/4", text: "Автовокзал" },
-      { to: "/5", text: "Права доступа" },
-    ],
-    reporting: [{ to: "/6", text: "Отчёты" }],
-    reference: [{ to: "/7", text: "Справочная" }],
-  },
+const useAsideStore = create<Store>()(
+  devtools(
+    persist(
+      immer((set, get) => {
+        const setActiveLinkByRoute = () => {
+          const currentPath = window.location.pathname;
+          const links = get().links;
+        
+          for (const key in links) {
+            const foundLink = links[key].find((link) => {
+              const linkPath = new URL(link.to, window.location.origin).pathname;
+              return linkPath === currentPath;
+            });
+            
+            if (foundLink) {
+              set({ activeLink: foundLink.text });
+              break;
+            }
+          }
+        };
 
-  linksNames: {
-    cashier: "Кассир",
-    dispatcher: "Диспетчер",
-    administrator: "Администратор",
-    reporting: "Модуль отчётности",
-    reference: "Справочная",
-  },
+        return {
+          links: {
+            cashier: [
+              { to: "/home/sale-ticket?step=0", text: "Продажа билетов" },
+              { to: "/home/refund-ticket", text: "Возврат билетов" },
+            ],
+            dispatcher: [{ to: "/3", text: "Управление рейсами" }],
+            administrator: [
+              { to: "/4", text: "Автовокзал" },
+              { to: "/5", text: "Права доступа" },
+            ],
+            // reporting: [{ to: "/6", text: "Отчёты" }],
+            reference: [{ to: "/7", text: "Справочная" }],
+          },
 
-  activeLink: null,
-  selectLink: (text) => {
-    const links = get().links;
-    const currentLink = Object.values(links)
-      .flat()
-      .find((link) => link.text === text);
-    if (currentLink) {
-      set({ activeLink: currentLink.text });
-    }
-  },
-}));
+          linksNames: {
+            cashier: "Кассир",
+            dispatcher: "Диспетчер",
+            administrator: "Администратор",
+            reporting: "Модуль отчётности",
+            reference: "Справочная",
+          },
+
+          activeLink: null,
+          selectLink: (text) => {
+            const links = get().links;
+            const currentLink = Object.values(links)
+              .flat()
+              .find((link) => link.text === text);
+            if (currentLink) {
+              set({ activeLink: currentLink.text });
+            }
+          },
+          setActiveLinkByRoute,
+        };
+      }),
+      {
+        name: "AsidePanelStore",
+        version: 1,
+        storage: createJSONStorage(() => sessionStorage),
+        partialize: (state) => ({ activeLink: state.activeLink }),
+        onRehydrateStorage: () => (state) => {
+          if (!state?.activeLink) {
+            state?.setActiveLinkByRoute?.();
+          }
+        },
+      },
+    ),
+    { name: "AsidePanelStore" },
+  ),
+);
 
 export { useAsideStore };
