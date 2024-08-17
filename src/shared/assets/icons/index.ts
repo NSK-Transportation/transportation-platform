1bddbb5a1f7e16b6a34d9f06ac9457e6cd0bb022
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-08-17 18:50:51 +0900

    feat(assets): add icons

--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -3,3 +3,4 @@ export { PiSeat as SeatIcon } from "react-icons/pi";
 export { FiUser as UserIcon } from "react-icons/fi";
 export { IoIosReturnLeft as ReturnIcon } from "react-icons/io";
 export { MdOutlinePayments as PaymentIcon } from "react-icons/md";
+export { BsCash as CashReturnIcon } from "react-icons/bs";
