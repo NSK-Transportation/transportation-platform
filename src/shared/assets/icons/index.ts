7fd7234db7ef9de30802712e5fe37803eb8fbbb6
armoney7 <arman.muradyan.2003@gmail.com>
2024-08-17 15:05:38 +0700

    add eye icons to icons file

--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -3,3 +3,5 @@ export { PiSeat as SeatIcon } from "react-icons/pi";
 export { FiUser as UserIcon } from "react-icons/fi";
 export { IoIosReturnLeft as ReturnIcon } from "react-icons/io";
 export { MdOutlinePayments as PaymentIcon } from "react-icons/md";
+export { GoEye as EyeOpenIcon } from "react-icons/go";
+export { GoEyeClosed as EyeCloseIcon } from "react-icons/go";
