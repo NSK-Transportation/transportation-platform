08ad6fd8163cfe68ef30ec14024985400bdd6d82
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-08-25 23:25:54 +0900

    fix(utils): add another format date

--- src/shared/utils/getFormatDate.ts
+++ src/shared/utils/getFormatDate.ts
@@ -19,3 +19,10 @@ export const getFormatDate = (date: string, format: "day" | "month" | "year"): s
       return "";
   }
 };
+
+export const formatDate = (date: Date): string => {
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const year = date.getFullYear();
+  return `${day}.${month}.${year}`;
+};
