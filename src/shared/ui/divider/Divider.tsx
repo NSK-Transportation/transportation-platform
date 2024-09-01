072660a3041efeda0f1321f6c5e4c1c682c3ced0
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-09-01 18:12:06 +0900

    fix(ui): styling & refactor

--- src/shared/ui/divider/Divider.tsx
+++ src/shared/ui/divider/Divider.tsx
@@ -1,15 +1,26 @@
-import { forwardRef, HTMLAttributes } from "react";
+import { CSSProperties, forwardRef, HTMLAttributes } from "react";
 import clsx from "clsx";
 import styles from "./Divider.module.scss";
 
 export interface DividerProps extends HTMLAttributes<HTMLDivElement> {
   orientation?: "horizontal" | "vertical";
+  color?: "default" | "blue";
+  width?: CSSProperties["borderWidth"];
   className?: string;
 }
 
 const Divider = forwardRef<HTMLDivElement, DividerProps>(
-  ({ orientation = "horizontal", className }, ref) => (
-    <div ref={ref} className={clsx(styles.divider, styles[orientation], className)} />
+  ({ orientation = "horizontal", color = "default", width, className }, ref) => (
+    <div
+      ref={ref}
+      className={clsx(styles.divider, className, {
+        [styles[color]]: color,
+        [styles[orientation]]: orientation,
+      })}
+      style={{
+        borderWidth: width,
+      }}
+    />
   ),
 );
 
