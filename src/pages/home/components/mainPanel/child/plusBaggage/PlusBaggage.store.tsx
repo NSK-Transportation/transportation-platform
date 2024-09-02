f1d329ddef0245815ccb7f9ca6e3cbe1a72f19a8
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-09-03 03:04:22 +0900

    fix(store): replace child store from main panel store

--- src/pages/home/components/mainPanel/child/plusBaggage/PlusBaggage.store.tsx
+++ src/pages/home/components/mainPanel/child/plusBaggage/PlusBaggage.store.tsx
@@ -1,11 +1,33 @@
+import { Passenger } from "@/app/@types";
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
 
 // Интерфейс хранилища
-export interface PlusBaggageStore {}
+export interface Store {
+  passenger: Required<Passenger>;
+  setPassenger: (data: Partial<Passenger>) => void;
+}
 
-export const usePlusBaggage = create<PlusBaggageStore>()(
+export const usePlusBaggage = create<Store>()(
   immer((set) => ({
-    // TODO:  Добавить состояния
+    passenger: {
+      id: 0,
+      firstName: "",
+      lastName: "",
+      patronymic: "",
+      gender: null,
+      birthday: "",
+      phone: "",
+      identification: null,
+      ticket: {
+        there: {},
+        return: {},
+      },
+    },
+
+    setPassenger: (data) =>
+      set((state) => {
+        state.passenger = { ...state.passenger, ...data };
+      }),
   })),
 );
--- src/pages/home/components/mainPanel/child/refundTicket/RefundTicket.store.tsx
+++ src/pages/home/components/mainPanel/child/refundTicket/RefundTicket.store.tsx
@@ -3,39 +3,33 @@ import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
 
 // Интерфейс хранилища
-export interface RefundTicketStore {
-  refundTicket: {
-    passenger: Required<Passenger>;
-    reasons: Refund[];
-    setPassenger: (data: Partial<Passenger>) => void;
-  };
+export interface Store {
+  passenger: Required<Passenger>;
+  reasons: Refund[];
+  setPassenger: (data: Partial<Passenger>) => void;
 }
 
-export const useRefundTicket = create<RefundTicketStore>()(
+export const useRefundTicket = create<Store>()(
   immer((set) => ({
-    refundTicket: {
-      passenger: {
-        id: 0,
-        firstName: "",
-        lastName: "",
-        patronymic: "",
-        gender: null,
-        birthday: "",
-        phone: "",
-        identification: null,
-        ticket: {
-          there: {},
-          return: {},
-        },
-        returnTicket: {},
+    passenger: {
+      id: 0,
+      firstName: "",
+      lastName: "",
+      patronymic: "",
+      gender: null,
+      birthday: "",
+      phone: "",
+      identification: null,
+      ticket: {
+        there: {},
+        return: {},
       },
-      reasons: [{ id: 1, type: "delay", rus: "Опоздание" }],
-
-      setPassenger: (data) =>
-        set((state) => {
-          state.refundTicket.passenger = { ...state.refundTicket.passenger, ...data };
-        }),
     },
+    reasons: [{ id: 1, type: "delay", rus: "Опоздание" }],
+
+    setPassenger: (data) =>
+      set((state) => {
+        state.passenger = { ...state.passenger, ...data };
+      }),
   })),
 );
-
