d68d7fe5d155f2d1f5ca3d3a1bf51c4842db194b
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-09-13 19:30:36 +0900

    fix(store)

--- src/pages/home/components/informationPanel/InformationPanel.store.tsx
+++ src/pages/home/components/informationPanel/InformationPanel.store.tsx
@@ -1,6 +1,16 @@
-import { Passenger, WayDetails } from "@/app/@types";
+import {
+  Baggage,
+  Discount,
+  Document,
+  Gender,
+  Passenger,
+  Payment,
+  Privilege,
+  Ticket,
+  WayDetails,
+} from "@/app/@types";
 import { create } from "zustand";
-import { devtools, subscribeWithSelector } from "zustand/middleware";
+import { devtools } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 import { useSaleTicket } from "../mainPanel";
 
@@ -11,6 +21,16 @@ export interface Store {
     return: WayDetails | null;
   };
   passengers: Passenger[];
+  options: {
+    tickets: Partial<Ticket>[];
+    discounts_main: Discount[];
+    discounts_child: Discount[];
+    baggages: Baggage[];
+    documents: Document[];
+    privileges: Privilege[];
+    payments: Payment[];
+    genders: Gender[];
+  };
 }
 
 export const useInformationStore = create<Store>()(
@@ -21,6 +41,16 @@ export const useInformationStore = create<Store>()(
         return: useSaleTicket.getState().activeWay.return,
       },
       passengers: useSaleTicket.getState().passengers,
+      options: {
+        tickets: useSaleTicket.getState().tickets,
+        discounts_main: useSaleTicket.getState().discount.main,
+        discounts_child: useSaleTicket.getState().discount.child,
+        baggages: useSaleTicket.getState().baggages,
+        documents: useSaleTicket.getState().documents,
+        privileges: useSaleTicket.getState().privileges,
+        payments: useSaleTicket.getState().payments,
+        genders: useSaleTicket.getState().genders,
+      },
     })),
     {
       name: "InformationPanel",
@@ -35,5 +65,15 @@ useSaleTicket.subscribe((state) => {
       return: state.activeWay.return,
     },
     passengers: state.passengers,
+    options: {
+      tickets: state.tickets,
+      discounts_main: state.discount.main,
+      discounts_child: state.discount.child,
+      baggages: state.baggages,
+      documents: state.documents,
+      privileges: state.privileges,
+      payments: state.payments,
+      genders: state.genders,
+    },
   });
 });
