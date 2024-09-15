43fd5cc6cdf2b4f1a0f53928e5f3469b7c37445e
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-09-15 15:37:29 +0900

    fix(ui): styling & refactor

--- src/shared/ui/box/Box.tsx
+++ src/shared/ui/box/Box.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, HTMLAttributes, ReactNode } from "react";
+import { CSSProperties, forwardRef, HTMLAttributes, ReactNode } from "react";
 import clsx from "clsx";
 import styles from "./Box.module.scss";
 
@@ -6,8 +6,13 @@ interface BoxProps extends HTMLAttributes<HTMLDivElement> {
   className?: string;
   children?: ReactNode;
   direction?: "up" | "down" | "left" | "right" | "center";
-  variant?: "default" | "dashed" | "solid";
+  variant?: "default" | "dashed" | "solid" | "withoutShadow";
   border?: "right" | "left" | "down" | "up" | "default";
+  padding?: boolean | number | [number, number, number, number];
+  cursor?: CSSProperties["cursor"];
+  overflow?: CSSProperties["overflow"];
+  hover?: boolean;
+  disabled?: boolean;
   text?: string;
   fullWidth?: boolean;
   color?: "blue" | "white";
@@ -22,6 +27,11 @@ const Box = forwardRef<HTMLDivElement, BoxProps>(
       color = "white",
       children,
       border = "default",
+      padding = true,
+      cursor,
+      overflow,
+      hover = false,
+      disabled = false,
       text,
       fullWidth,
       ...props
@@ -36,7 +46,20 @@ const Box = forwardRef<HTMLDivElement, BoxProps>(
         [styles[color]]: color,
         [styles[`border-${border}`]]: border,
         [styles.fullWidth]: fullWidth,
+        [styles.hover]: hover,
+        [styles.disabled]: disabled,
       })}
+      style={{
+        padding: Array.isArray(padding)
+          ? `${padding[0]}px ${padding[1]}px ${padding[2]}px ${padding[3]}px`
+          : typeof padding === "boolean"
+            ? padding
+              ? "16px"
+              : "0px"
+            : `${padding}px`,
+        cursor: cursor,
+        overflow: overflow,
+      }}
       {...props}
     >
       {text && <span className={styles.box__text}>{text}</span>}
