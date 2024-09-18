e034fe67d0dccd0c3528bfeeb68be3e4cc7a0698
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-09-18 19:34:48 +0900

    feat(ui): adding country select
    Usage for slots in "Input" or in another ui components

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -31,3 +31,4 @@ export { User } from "./user/User";
 export { Grid } from "./grid/Grid";
 export { CustomCalendar as Calendar } from "./customCalendar/CustomCalendar";
 export { Tooltip } from "./tooltip/Tooltip";
+export { CountrySelect } from "./countrySelect/CountrySelect";
