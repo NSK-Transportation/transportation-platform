b8f06ee38f4906de6b37167e06064fb1c3adc908
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-10-11 22:45:49 +0900

    fix(pages\sale ticket): adding steps validation

--- src/pages/SaleTicket/ui/SaleTicket.tsx
+++ src/pages/SaleTicket/ui/SaleTicket.tsx
@@ -1,24 +1,10 @@
-import { StepperButtons } from "@/features/step";
-import { Stacks, Stepper } from "@/shared/ui";
-import { StepContent } from "../lib/data/StepContent";
-import { useSaleTicketSteps } from "../model/hooks/useSaleTicketSteps";
+import { Stacks } from "@/shared/ui";
+import { StepContent } from "./StepContent";
 
 export const SaleTicket = () => {
-  const { activeStep, steps } = useSaleTicketSteps();
-  const { handleNextStep, handlePrevStep, isFirstStep, isLastStep } = useSaleTicketSteps();
-
   return (
     <Stacks gap={16} direction="column" fullheight>
-      <Stepper direction="row" activeStep={activeStep} steps={steps} />
-
-      <StepContent activeStep={activeStep} />
-
-      <StepperButtons
-        handlePrevStep={handlePrevStep}
-        handleNextStep={handleNextStep}
-        isFirstStep={isFirstStep}
-        isLastStep={isLastStep}
-      />
+      <StepContent />
     </Stacks>
   );
 };
