030b73629489ade61c9e73c091cb9a4febedfed0
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-10-14 21:03:06 +0900

    fix(types)

--- src/entities/payment/model/types/payment.types.ts
+++ src/entities/payment/model/types/payment.types.ts
@@ -3,5 +3,42 @@ import { Options } from "@/shared/types";
 export type PaymentType = "cash" | "card" | "sbp";
 
 export interface Payment extends Options<PaymentType, "type"> {
+  ticket?: {
+    amount: number;
+    there: {
+      price: number;
+    };
+    return: {
+      price: number;
+    };
+  };
+  baggage?: {
+    amount: number;
+    quantity: number;
+    there: {
+      price: number;
+    };
+    return: {
+      price: number;
+    };
+  };
   amount?: number;
+}
+
+export interface GetPaymentResponse {
+  id: number;
+  status: string;
+  paid: boolean;
+  amount: {
+    value: number;
+    currency: string;
+  };
+  confirmation: {
+    type: string;
+    confirmationUrl: string;
+    failUrl: string;
+  };
+  created_at: Date;
+  description: string;
+  refundable: boolean;
 }
