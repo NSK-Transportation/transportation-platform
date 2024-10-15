961738930da3ebd8d15f8e20b4be955106746f4e
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-10-15 16:27:30 +0900

    fix(ui)

--- src/shared/ui/layout/Layout.tsx
+++ src/shared/ui/layout/Layout.tsx
@@ -5,38 +5,16 @@ import styles from "./Layout.module.scss";
 export interface LayoutProps extends HTMLAttributes<HTMLDivElement> {
   asidePanel: ReactNode; // Левая часть интерфейса
   mainPanel: ReactNode; // Серединая часть интерфейса
-  informationPanel: ReactNode; // Правая часть интерфейса
   headerPanel: ReactNode; // Верхняя часть интерфейса
-  gridTemplateAreas?: "default" | "withoutInfo";
   className?: string;
 }
 
 const Layout = forwardRef<HTMLDivElement, LayoutProps>(
-  (
-    {
-      asidePanel,
-      mainPanel,
-      informationPanel,
-      headerPanel,
-      gridTemplateAreas = "default",
-      className,
-      ...props
-    },
-    ref,
-  ) => (
-    <div
-      ref={ref}
-      className={clsx(styles.layout, className, {
-        [styles[gridTemplateAreas]]: gridTemplateAreas,
-      })}
-      {...props}
-    >
+  ({ asidePanel, mainPanel, headerPanel, className, ...props }, ref) => (
+    <div ref={ref} className={clsx(styles.layout, className)} {...props}>
       <header className={styles.layout__headerPanel}>{headerPanel}</header>
       <aside className={styles.layout__asidePanel}>{asidePanel}</aside>
       <main className={styles.layout__mainPanel}>{mainPanel}</main>
-      {gridTemplateAreas !== "withoutInfo" && (
-        <section className={styles.layout__informationPanel}>{informationPanel}</section>
-      )}
     </div>
   ),
 );
