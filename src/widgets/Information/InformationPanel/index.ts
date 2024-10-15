f2780f70d457cfd126a97730d39f6482004033c2
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-10-15 16:28:29 +0900

    feat(widgets\information): added information panel

---
+++ src/widgets/Information/InformationPanel/index.ts
@@ -0,0 +1,1 @@
+export { InformationPanel } from "./ui/InformationPanel";
---
+++ src/widgets/Information/InformationPanel/ui/InformationPanel.tsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import { PassengerTicketInfo, usePassengerStore } from "@/entities/passenger";
+import { usePaymentStore } from "@/entities/payment";
+import { SeatPlace } from "@/entities/seat";
+import { WayInfo } from "@/entities/way";
+import { useWayDetailStore } from "@/entities/wayDetails";
+import { ArrowDownIcon, ArrowUpIcon } from "@/shared/assets";
+import { Box, Stacks, Typography } from "@/shared/ui";
+
+export const InformationPanel = () => {
+  const { passengers } = usePassengerStore();
+  const { activeWay } = useWayDetailStore();
+  const { payment } = usePaymentStore();
+  const [visible, setVisible] = useState(true);
+
+  if (!(activeWay.there ?? activeWay.return)) {
+    return <Box style={{ height: "100%" }} text="Данных нет" />;
+  }
+
+  const selectedSeats = activeWay?.there?.seats?.filter((seat) => seat.status === "selected");
+
+  return (
+    <Stacks direction="column">
+      <Box border="up">
+        <Stacks direction="column" gap={16}>
+          <WayInfo activeWay={activeWay.there} />
+
+          {selectedSeats && selectedSeats?.length > 0 && (
+            <Typography
+              onClick={() => setVisible(!visible)}
+              cursor="pointer"
+              variant="h4"
+              color="secondary"
+            >
+              <Stacks alignItems="center" gap={2}>
+                Детали рейса
+                {visible ? <ArrowDownIcon /> : <ArrowUpIcon />}
+              </Stacks>
+            </Typography>
+          )}
+
+          {visible &&
+            passengers.map((passenger) => (
+              <Box key={passenger.id} variant="dashed">
+                <Stacks direction="column" gap={16}>
+                  {passenger.ticket.there?.wayDetail?.id && (
+                    <SeatPlace
+                      passenger={passenger}
+                      direction={"there"}
+                      passengerInfo={
+                        passenger.ticket.there.type && (
+                          <PassengerTicketInfo passenger={passenger} direction={"there"} />
+                        )
+                      }
+                    />
+                  )}
+                  {passenger.ticket.return && (
+                    <SeatPlace
+                      passenger={passenger}
+                      direction={"return"}
+                      passengerInfo={
+                        passenger.ticket.return.type && (
+                          <PassengerTicketInfo passenger={passenger} direction={"return"} />
+                        )
+                      }
+                      wayInfo={activeWay.return && <WayInfo activeWay={activeWay.return} />}
+                    />
+                  )}
+                </Stacks>
+              </Box>
+            ))}
+        </Stacks>
+      </Box>
+      <Box color="blue" border="down">
+        <Stacks justifyContent="flex-end">
+          <Typography variant="h1" color="default-white">
+            {payment.amount} руб
+          </Typography>
+        </Stacks>
+      </Box>
+    </Stacks>
+  );
+};
