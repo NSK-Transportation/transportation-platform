542187f27569819abe50dd49ebb75a6b111f9aae
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-10-15 16:28:53 +0900

    fix(widgets\passenger)

--- src/widgets/Passenger/passengerCard/ui/PassengerCard.tsx
+++ src/widgets/Passenger/passengerCard/ui/PassengerCard.tsx
@@ -16,39 +16,49 @@ export const PassengerCard: FC<Props> = ({ direction }) => {
 
   return (
     <>
-      {passengers.map((passenger) => (
-        <Box key={passenger.id}>
-          <Stacks direction="column" gap={16}>
-            <Stacks gap={8}>
-              <Typography variant="h3">Данные пассажира </Typography>
-              <Chip
-                variant="primary"
-                size="small"
-                label={`Место ${passenger.ticket[direction]?.seatId}`}
-              />
-            </Stacks>
-
-            <Grid gap={16}>
-              <Grid columns="repeat(2, 1fr)" gap={16}>
-                <PassengerFormField
-                  direction={direction}
-                  passenger={passenger}
-                  addBaggage={
-                    <AddBaggagePopover
-                      baggage={baggage}
-                      passenger={passenger}
-                      direction={direction}
-                    />
-                  }
+      {passengers
+        .filter((passenger) => {
+          if (direction === "there") {
+            return true;
+          }
+          if (direction === "return") {
+            return passenger.ticket.return !== null;
+          }
+          return false;
+        })
+        .map((passenger) => (
+          <Box key={passenger.id}>
+            <Stacks direction="column" gap={16}>
+              <Stacks gap={8}>
+                <Typography variant="h3">Данные пассажира </Typography>
+                <Chip
+                  variant="primary"
+                  size="small"
+                  label={`Место ${passenger.ticket[direction]?.seatId}`}
                 />
+              </Stacks>
+
+              <Grid gap={16}>
+                <Grid columns="repeat(2, 1fr)" gap={16}>
+                  <PassengerFormField
+                    direction={direction}
+                    passenger={passenger}
+                    addBaggage={
+                      <AddBaggagePopover
+                        baggage={baggage}
+                        passenger={passenger}
+                        direction={direction}
+                      />
+                    }
+                  />
+                </Grid>
               </Grid>
-            </Grid>
-            <Typography variant="h4" color="secondary">
-              * - обязательно к заполнению
-            </Typography>
-          </Stacks>
-        </Box>
-      ))}
+              <Typography variant="h4" color="secondary">
+                * - обязательно к заполнению
+              </Typography>
+            </Stacks>
+          </Box>
+        ))}
     </>
   );
 };
