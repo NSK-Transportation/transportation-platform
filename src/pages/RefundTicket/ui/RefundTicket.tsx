a40a6da8f7b90c7de5259deff93b72077c17dd5f
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-10-19 18:00:12 +0900

    fix(pages\refund-ticket)

--- src/pages/RefundTicket/ui/RefundTicket.tsx
+++ src/pages/RefundTicket/ui/RefundTicket.tsx
@@ -1,10 +1,14 @@
 import { Refund } from "@/widgets/Refund";
 import { PassengerSearchForm } from "@/features/passenger";
-import { PassengerInformation } from "@/entities/passenger";
+import { PassengerInformation, usePassengerStore } from "@/entities/passenger";
 import { TicketInformation } from "@/entities/ticket";
+import { WayInfo } from "@/entities/way";
+import { WayDetail } from "@/entities/wayDetails";
 import { Grid, Stacks } from "@/shared/ui";
 
 export const RefundTicket = () => {
+  const { passenger } = usePassengerStore();
+
   return (
     <Grid columns="2fr 1fr" gap={16}>
       <Grid rows="span">
@@ -17,7 +21,10 @@ export const RefundTicket = () => {
       </Grid>
       <Grid columns="2fr 3fr" gap={16}>
         <PassengerInformation />
-        <TicketInformation />
+        <TicketInformation
+          wayInfoThere={<WayInfo activeWay={passenger?.ticket.there?.wayDetail as WayDetail} />}
+          wayInfoReturn={<WayInfo activeWay={passenger?.ticket.return?.wayDetail as WayDetail} />}
+        />
       </Grid>
     </Grid>
   );
