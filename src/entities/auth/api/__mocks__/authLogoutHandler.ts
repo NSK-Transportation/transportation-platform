6bdf666dd676fd8bd5c9749e9514cfbbca03f400
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-10-19 17:34:55 +0900

    fix(entities\auth)

--- src/entities/auth/api/__mocks__/authLogoutHandler.ts
+++ src/entities/auth/api/__mocks__/authLogoutHandler.ts
@@ -1,5 +1,5 @@
 import { HttpResponse, http } from "msw";
 
-export default http.get(import.meta.env.API_URL + "/logout", () => {
+export default http.get(import.meta.env.VITE_API_URL + "/logout", () => {
   return HttpResponse.text("", { status: 200 });
 });
