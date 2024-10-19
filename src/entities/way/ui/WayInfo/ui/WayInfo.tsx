f576401af8cf33e3160dbb21aa926247e8e3068f
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2024-10-19 17:59:10 +0900

    fix(entities\way)

--- src/entities/way/ui/WayInfo/ui/WayInfo.tsx
+++ src/entities/way/ui/WayInfo/ui/WayInfo.tsx
@@ -1,68 +1,76 @@
 /* eslint-disable @conarti/feature-sliced/layers-slices */
-import { FC } from "react";
+import { FC, ReactNode } from "react";
 import { WayDetail } from "@/entities/wayDetails";
-import { Stacks, Typography } from "@/shared/ui";
+import { Divider, Stacks, Typography } from "@/shared/ui";
 import { Tooltip } from "@/shared/ui";
 
 interface Props {
   activeWay: WayDetail | null;
+  actions?: ReactNode;
+  typography?: ReactNode;
+  border?: boolean;
 }
 
-export const WayInfo: FC<Props> = ({ activeWay }) => {
+export const WayInfo: FC<Props> = ({ activeWay, actions, border, typography }) => {
   return (
-    <Stacks gap={8} direction="column" fullwidth>
-      <Stacks gap={4} direction="row" justifyContent="space-between">
-        <Stacks gap={4} direction="row">
-          <Typography variant="h3" weight={600}>
-            Рейс №{activeWay?.wayNumber}
-          </Typography>
-          <Typography variant="h3" weight={600}>
-            {activeWay?.from.city.rus} - {activeWay?.to.city.rus}
-          </Typography>
+    <Stacks gap={8}>
+      {border && <Divider orientation="vertical" color="blue" width={2} />}
+      <Stacks gap={8} direction="column" fullwidth>
+        {typography}
+        <Stacks gap={4} direction="row" justifyContent="space-between">
+          <Stacks gap={4} direction="row">
+            <Typography variant="h3" weight={600}>
+              Рейс №{activeWay?.wayNumber}
+            </Typography>
+            <Typography variant="h3" weight={600}>
+              {activeWay?.from.city.rus} - {activeWay?.to.city.rus}
+            </Typography>
+          </Stacks>
+          <Tooltip text={activeWay?.whoArive ?? ""} direction="down">
+            <Typography cursor="pointer" color="secondary" variant="h4">
+              Перевозчик
+            </Typography>
+          </Tooltip>
         </Stacks>
-        <Tooltip text={activeWay?.whoArive ?? ""} direction="down">
-          <Typography cursor="pointer" color="secondary" variant="h4">
-            Перевозчик
-          </Typography>
-        </Tooltip>
-      </Stacks>
-      <Stacks gap={32} direction="row">
-        <Stacks direction="column">
-          <Stacks gap={4} alignItems="flex-end">
-            <Typography variant="h1" weight={600} color="primary-second">
-              {activeWay?.from.time}
-            </Typography>
-            <Typography variant="h3" weight={600} color="primary">
-              {activeWay?.from.date}
-            </Typography>
+        <Stacks gap={32} direction="row">
+          <Stacks direction="column">
+            <Stacks gap={4} alignItems="flex-end">
+              <Typography variant="h1" weight={600} color="primary-second">
+                {activeWay?.from.time}
+              </Typography>
+              <Typography variant="h3" weight={600} color="primary">
+                {activeWay?.from.date}
+              </Typography>
+            </Stacks>
+            <Stacks direction="column">
+              <Typography variant="h3" color="secondary">
+                {activeWay?.from.city.rus} - {activeWay?.from.station.rus}
+              </Typography>
+              <Typography variant="h3" color="secondary">
+                {activeWay?.from.street}, {activeWay?.from.house}
+              </Typography>
+            </Stacks>
           </Stacks>
           <Stacks direction="column">
-            <Typography variant="h3" color="secondary">
-              {activeWay?.from.city.rus} - {activeWay?.from.station.rus}
-            </Typography>
-            <Typography variant="h3" color="secondary">
-              {activeWay?.from.street}, {activeWay?.from.house}
-            </Typography>
+            <Stacks gap={4} alignItems="flex-end">
+              <Typography variant="h1" weight={600} color="primary-second">
+                {activeWay?.to.time}
+              </Typography>
+              <Typography variant="h3" weight={600} color="primary">
+                {activeWay?.to.date}
+              </Typography>
+            </Stacks>
+            <Stacks direction="column">
+              <Typography variant="h3" color="secondary">
+                {activeWay?.to.city.rus} - {activeWay?.to.station.rus}
+              </Typography>
+              <Typography variant="h3" color="secondary">
+                {activeWay?.to.street}, {activeWay?.to.house}
+              </Typography>
+            </Stacks>
           </Stacks>
         </Stacks>
-        <Stacks direction="column">
-          <Stacks gap={4} alignItems="flex-end">
-            <Typography variant="h1" weight={600} color="primary-second">
-              {activeWay?.to.time}
-            </Typography>
-            <Typography variant="h3" weight={600} color="primary">
-              {activeWay?.to.date}
-            </Typography>
-          </Stacks>
-          <Stacks direction="column">
-            <Typography variant="h3" color="secondary">
-              {activeWay?.to.city.rus} - {activeWay?.to.station.rus}
-            </Typography>
-            <Typography variant="h3" color="secondary">
-              {activeWay?.to.street}, {activeWay?.to.house}
-            </Typography>
-          </Stacks>
-        </Stacks>
+        {actions}
       </Stacks>
     </Stacks>
   );
